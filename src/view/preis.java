59d31edcb7692f06e810dbb1bae76ea3f917082b
PJ <PJ@Praveen>
2017-09-01 16:30:29 +0200

    gui erstellt

--- src/view/preis.java
+++ src/view/preis.java
@@ -6,10 +6,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class preis extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,12 +35,25 @@ public class preis extends JFrame {
 	 * Create the frame.
 	 */
 	public preis() {
+		setTitle("Preisabsatzfunktion");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 519, 385);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Artikel Suchen:");
+		lblNewLabel.setBounds(20, 22, 93, 20);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(20, 77, 143, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblArtikelnamenEingeben = new JLabel("Artikel-ID eingeben:");
+		lblArtikelnamenEingeben.setBounds(20, 53, 130, 14);
+		contentPane.add(lblArtikelnamenEingeben);
 	}
-
 }
