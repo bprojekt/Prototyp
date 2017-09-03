692cff320fdfb7e5fcd358828cab4eeeb71ce8bd
PJ <PJ@Praveen>
2017-09-03 17:29:13 +0200

    getPreis aufgestellt

--- src/view/preis.java
+++ src/view/preis.java
@@ -18,6 +18,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.sql.*;
 import java.util.ArrayList;
+import java.awt.Font;
+import javax.swing.JTabbedPane;
 
 public class preis extends JFrame {
 
@@ -26,7 +28,10 @@ public class preis extends JFrame {
 	JLabel artname;
 	JLabel paf;
 	double [][] data;
+	JLabel prize ;
 	ArrayList<Artikel> artikel= new ArrayList<Artikel>();
+	SimpleRegression reg= new SimpleRegression();
+	private JTextField textField_1;
 	/**
 	 * Launch the application.
 	 */
@@ -49,22 +54,24 @@ public class preis extends JFrame {
 	public preis() {
 		setTitle("Preisabsatzfunktion");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 519, 450);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Artikel Suchen:");
-		lblNewLabel.setBounds(20, 22, 93, 20);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(20, 22, 130, 20);
 		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(20, 78, 143, 20);
+		textField.setBounds(20, 79, 143, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblArtikelnamenEingeben = new JLabel("Artikel-ID eingeben:");
+		lblArtikelnamenEingeben.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblArtikelnamenEingeben.setBounds(20, 53, 130, 14);
 		contentPane.add(lblArtikelnamenEingeben);
 		
@@ -75,26 +82,65 @@ public class preis extends JFrame {
 				
 			}
 		});
-		btnNewButton.setBounds(173, 76, 89, 23);
+		btnNewButton.setBounds(173, 78, 89, 23);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblArtikel = new JLabel("Artikel:");
-		lblArtikel.setBounds(20, 122, 46, 14);
+		lblArtikel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblArtikel.setBounds(20, 132, 46, 14);
 		contentPane.add(lblArtikel);
 		
 		JLabel artname = new JLabel("New label");
-		artname.setBounds(76, 122, 268, 14);
+		artname.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		artname.setBounds(76, 132, 285, 14);
 		contentPane.add(artname);
 		artname.setVisible(false);
 		
 		JLabel lblNewLabel_1 = new JLabel("Preisabsatzfunktion:");
-		lblNewLabel_1.setBounds(20, 157, 130, 14);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(20, 193, 130, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel paf = new JLabel("New label");
-		paf.setBounds(173, 157, 108, 14);
+		paf.setBounds(161, 194, 191, 14);
 		contentPane.add(paf);
 		paf.setVisible(false);
+		
+		JLabel lblNewLabel_2 = new JLabel("Preis bestimmen:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2.setBounds(450, 27, 143, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblMengeEingeben = new JLabel("Menge eingeben:");
+		lblMengeEingeben.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblMengeEingeben.setBounds(450, 82, 118, 14);
+		contentPane.add(lblMengeEingeben);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(556, 79, 112, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblPreis = new JLabel("Preis:");
+		lblPreis.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblPreis.setBounds(450, 118, 94, 14);
+		contentPane.add(lblPreis);
+		
+		prize= new JLabel("0.00");
+		prize.setBounds(556, 119, 46, 14);
+		contentPane.add(prize);
+		
+		JButton btnNewButton_1 = new JButton("Ermitteln");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(textField_1.getText().length()>0)
+				prize.setText(""+getPreis(textField_1.getText())+"");
+			}
+		});
+		btnNewButton_1.setBounds(678, 78, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		
 	}
 	
 	public void  table(String s){
@@ -169,13 +215,33 @@ public class preis extends JFrame {
 	}
 	
 	public void regression(double [][]data){
-		SimpleRegression reg= new SimpleRegression();
+		
 		reg.addData(data);
 		double abs= Math.round(reg.getIntercept()*100.0)/100.0;
 		double stg= Math.round(reg.getSlope()*100.0)/100.0;
-		paf.setText("q(p)= " + abs + " " +stg+ "*p" );
+		paf.setText("q(p)= " + abs + " " +stg+ "*p" ); 
+		paf.setVisible(true);
 		
-		
-		
+	}
+	
+	public double getPreis(String menge){
+		double p=0.00;
+		boolean a=true;
+		int m=0;
+		try{
+		m=Integer.parseInt(menge);
+		}catch (NumberFormatException e)
+		{
+			a=false;
+			JOptionPane.showMessageDialog(null,"Bitte geben sie nur ganzahlige Zahlen ein","Fehler",JOptionPane.ERROR_MESSAGE);
+			
+		}
+		if(a!=false){
+			double abs= Math.round(reg.getIntercept()*100.0)/100.0;
+			double stg= Math.round(reg.getSlope()*100.0)/100.0;
+			double i= (m-abs)/stg;
+			p = Math.round(i*100.0)/100.0;	
+		}
+		return p;
 	}
 }
